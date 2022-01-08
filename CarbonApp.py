64e81dd89173455ccc7fc244f7e4ede709b055b0
Daniel <76977155+sciDelta@users.noreply.github.com>
2022-01-08 15:20:27 +0000

    Update CarbonApp.py

--- CarbonApp.py
+++ CarbonApp.py
@@ -71,7 +71,7 @@ plt.ylabel('Generation %')
 plt.ylim(0,100)
 plt.legend(['Fossil Fuels', 'Nuclear', 'Renewables'])
 
-if start - end <=2:
+if started == ended:
     plt.title(f'{sel_box} Energy Mix \n{started}', size = 14)
     dtFmt = mdates.DateFormatter('%H:%M')
 else:
