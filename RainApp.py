a08e4a4e4ee40fd7618fd90fb80c54d60c4bd3c5
Daniel <76977155+sciDelta@users.noreply.github.com>
2022-02-10 19:13:29 +0000

    Update RainApp.py

--- RainApp.py
+++ RainApp.py
@@ -8,11 +8,14 @@ def source_data():
     url = 'https://raw.githubusercontent.com/sciDelta/API-ETL-SEPA-rainfall/main/data/SEPA_Monthly.csv'
 
     df = pd.read_csv(url, parse_dates=['Timestamp'])
-    df = df.drop(columns = ['Station_no', 'Station_number'])
     df['Year'] = [i.year for i in df['Timestamp']]
-    df['Rainfall'] = df['Rainfall'].astype(int)
+    
+    for col in [ 'Unnamed: 0', 'station_no', 'station_number']:
+        if col in df.columns:
+            del df[col]
 
     return df
+
 df = source_data()
 
 # Download converter
@@ -27,9 +30,9 @@ def time_stats(base_data):
     time_data = pd.DataFrame(index=dates, columns=['Min Rainfall','Mean Rainfall', 'Max Rainfall'])
 
     for i in time_data.index:
-        time_data.loc[i, 'Mean Rainfall'] = base_data[base_data['Timestamp'] == i]['Rainfall'].mean().astype(int)
-        time_data.loc[i, 'Max Rainfall'] = base_data[base_data['Timestamp'] == i]['Rainfall'].max().astype(int)
-        time_data.loc[i, 'Min Rainfall'] = base_data[base_data['Timestamp'] == i]['Rainfall'].min().astype(int)
+        time_data.loc[i, 'Mean Rainfall'] = base_data[base_data['Timestamp'] == i]['Rainfall (mm)'].mean().astype(int)
+        time_data.loc[i, 'Max Rainfall'] = base_data[base_data['Timestamp'] == i]['Rainfall (mm)'].max().astype(int)
+        time_data.loc[i, 'Min Rainfall'] = base_data[base_data['Timestamp'] == i]['Rainfall (mm)'].min().astype(int)
     
     return time_data
 
@@ -40,7 +43,7 @@ st.title('''Scottish Rainfall API Data''')
 col1, col2 = st.columns(2)
 
 filter_station = ['All']
-for i in df['Station Name'].sort_values(ascending=True).unique():
+for i in df['station_name'].sort_values(ascending=True).unique():
     filter_station.append(i)
 selected_station = col1.selectbox('Station', filter_station)
 
@@ -55,9 +58,9 @@ if selected_station == 'All' and selected_year == 'All':
 elif selected_station == 'All' and selected_year != 'All':
     map_data = df[df['Year'] == int(selected_year)].copy()
 elif selected_station != 'All' and selected_year == 'All':
-    map_data = df[df['Station Name'] == selected_station].copy()
+    map_data = df[df['station_name'] == selected_station].copy()
 else:
-    map_data = df[(df['Year'] == int(selected_year)) & (df['Station Name'] == selected_station)].copy()
+    map_data = df[(df['Year'] == int(selected_year)) & (df['station_name'] == selected_station)].copy()
 
 map_chart = map_data.copy().drop(columns=['Year'])
 
