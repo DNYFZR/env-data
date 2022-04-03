08586d4013940f1a9a13c4e4fa7f550f73e6309d
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-04-03 16:05:16 +0100

    Moved to src

---
+++ src/DataPipeline_v1.py
@@ -0,0 +1,87 @@
+#### Carbon Intensity API Pipeline ####
+import requests, pandas as pd, datetime as dt
+
+# Wrapper function for API extract
+def get_api_data(start_date, end_date):
+    if type(start_date) == str:
+        start_date = pd.to_datetime(start_date)
+    if type(end_date) == str:
+        end_date = pd.to_datetime(end_date)
+
+    api_url = 'https://api.carbonintensity.org.uk/regional/intensity/{}T00:30Z/{}T00:00Z'
+    headers = {'Accept': 'application/json'}
+
+    df_mix = pd.DataFrame()
+    date_list = list()
+
+    # Create list of dates for URL
+    if start_date == end_date:
+        date_list.append( (str(start_date), str(end_date + dt.timedelta(days = 1)) ) )
+    else:
+        i = start_date
+        while i < end_date:
+            first = i
+            last = i + dt.timedelta(days = 1) 
+            date_list.append( (str(first), str(last)) )
+            i += dt.timedelta(days = 1)
+
+    # Extract JSON from API
+    for i in range(len(date_list)):
+        url = api_url.format(date_list[i][0], date_list[i][1])
+        req = requests.get(url, params={}, headers = headers)
+
+        for j in range(48): # 48 half hours per day
+            if len(req.json()['data']) <= j: 
+                pass
+            else:
+                df = pd.DataFrame(req.json()['data'][j]['regions'])
+                df['intensity'] = [i['forecast'] for i in df['intensity']]
+
+                # Start & end timestamps
+                df['start date'] = req.json()['data'][j]['from']
+                df['end date'] = req.json()['data'][j]['to']
+
+                # Combine with main df
+                df_mix = pd.concat([df_mix, df], axis=0)
+
+    # Extract data from JSON
+    df_mix = df_mix.reset_index(drop = True)
+    biomass, coal, imports, gas, nuclear, other, hydro, solar, wind = {}, {}, {}, {}, {}, {}, {}, {}, {}
+
+    for i in df_mix.index:
+        biomass.update({i: df_mix['generationmix'][i][0]['perc']})
+        coal.update({i: df_mix['generationmix'][i][1]['perc']})
+        imports.update({i: df_mix['generationmix'][i][2]['perc']})
+        gas.update({i: df_mix['generationmix'][i][3]['perc']})
+        nuclear.update({i: df_mix['generationmix'][i][4]['perc']})
+        other.update({i: df_mix['generationmix'][i][5]['perc']})
+        hydro.update({i: df_mix['generationmix'][i][6]['perc']})
+        solar.update({i: df_mix['generationmix'][i][7]['perc']})
+        wind.update({i: df_mix['generationmix'][i][8]['perc']})
+
+    df_mix['biomass'] = df_mix.index.map(biomass)
+    df_mix['coal'] = df_mix.index.map(coal)
+    df_mix['imports'] = df_mix.index.map(imports)
+    df_mix['gas'] = df_mix.index.map(gas)
+    df_mix['nuclear'] = df_mix.index.map(nuclear)
+    df_mix['other'] = df_mix.index.map(other)
+    df_mix['hydro'] = df_mix.index.map(hydro)
+    df_mix['solar'] = df_mix.index.map(solar)
+    df_mix['wind'] = df_mix.index.map(wind) 
+
+    for col in df_mix.columns:
+        if col == 'generationmix':
+            del df_mix[col]
+
+    # Fix North Wales & Merseyside tag
+    df_mix['shortname'] = ['North Wales and Merseyside' if i == 'North Wales & Merseyside' else i for i in df_mix['shortname']]
+
+    return df_mix
+
+if __name__ == '__main__':
+    from time import time
+    run_start = time()
+    
+    df = get_api_data(dt.date(2022,1,1), dt.date(2022,4,1))
+    
+    print(f'Run time: {round(time() - run_start, 1)}s')
---
+++ src/DataPipeline_v2.py
@@ -0,0 +1,142 @@
+### Carbon Intensity API Data Pipeline ###
+import requests, pandas as pd, datetime as dt
+import time
+
+def extract_api_data( start_date = dt.date(2022,2,20), end_date = dt.date(2022,2,22) ):
+    '''
+    Function calls the API url and returns the JSON data structure between teh selected dates
+    '''
+
+    api_url ='https://api.carbonintensity.org.uk/regional/intensity/{}T00:30Z/{}T00:00Z'
+    api_headers = {'Accept': 'application/json'}
+    
+    date_list = list()
+
+    # Create list of dates for URL
+    if start_date == end_date:
+        date_list.append( (str(start_date), str(end_date + dt.timedelta(days = 1)) ) )
+    else:
+        i = start_date
+        while i < end_date:
+            first = i        
+            last = i + dt.timedelta(days = 1) 
+            date_list.append( (str(first), str(last)) )
+            i += dt.timedelta(days = 1)
+
+    # Extract JSON from API and append to list
+    api_json = list()
+    for i in range(len(date_list)):
+        url = api_url.format(date_list[i][0], date_list[i][1])
+        req = requests.get(url, params={}, headers = api_headers).json()
+        api_json.append(req)
+
+    return api_json
+
+def extract_json_data(dataset = extract_api_data()):
+    # Set up columns
+    start_dates, end_dates = list(), list()
+    data_nest = list()
+
+    # Extract dates from 1st level JSON
+    for json_nest in dataset:
+        json_nest = json_nest['data']
+
+        [start_dates.append(i['from']) for i in json_nest]
+        [end_dates.append(i['to']) for i in json_nest]
+        [data_nest.append(i['regions']) for i in json_nest]
+
+    # Create & update output table
+    output_table = pd.DataFrame()
+
+    output_table['Start'] = [pd.to_datetime(i) for i in start_dates]
+    output_table['End'] = [pd.to_datetime(i) for i in end_dates]
+    output_table['data']  = data_nest
+    
+    return output_table
+
+def transform_json(table = extract_json_data()):
+    # Set up containers
+    output = pd.DataFrame()
+    start, end, ids, names, intensity, energy = list(), list(), list(), list(), list(), list()
+
+    # Transform data structure
+    for row, data in enumerate(table['data'].copy()):
+        start_date = table.loc[row, 'Start']
+        end_date = table.loc[row, 'End']
+
+        for region in data:
+            start.append(start_date)
+            end.append(end_date)
+            ids.append(region['regionid'])
+            names.append(region['shortname'])
+            intensity.append(region['intensity']) 
+            energy.append(region['generationmix']) 
+
+    output['Start'] = start
+    output['End'] = end
+    output['ID'] = ids
+    output['Name'] = names
+    output['Intensity'] = [i['forecast'] for i in intensity]
+    output['Intensity Category'] = [i['index'] for i in intensity]
+    output['Energy Mix'] = energy
+    
+    return output
+
+def transform_json_energy(table = transform_json()):
+    # Set up containers for energy mix extraction
+    biomass,hydro, solar, wind = list(), list(), list(), list()
+    coal, gas = list(), list()
+    nuclear = list()
+    imports, other = list(), list()
+
+    energy = table['Energy Mix'].copy()
+    for mix in energy:
+        for idx in range(len(mix)):
+            if mix[idx]['fuel'] == 'biomass':
+                biomass.append(mix[idx]['perc'])
+            if mix[idx]['fuel'] == 'hydro':
+                hydro.append(mix[idx]['perc'])
+            if mix[idx]['fuel'] == 'solar':
+                solar.append(mix[idx]['perc'])
+            if mix[idx]['fuel'] == 'wind':
+                wind.append(mix[idx]['perc'])
+            
+            if mix[idx]['fuel'] == 'coal':
+                coal.append(mix[idx]['perc'])
+            if mix[idx]['fuel'] == 'gas':
+                gas.append(mix[idx]['perc'])
+            
+            if mix[idx]['fuel'] == 'nuclear':
+                nuclear.append(mix[idx]['perc'])
+            
+            if mix[idx]['fuel'] == 'imports':
+                imports.append(mix[idx]['perc'])
+            if mix[idx]['fuel'] == 'other':
+                other.append(mix[idx]['perc'])
+
+    table['Biomass %'] = biomass
+    table['Hydro %'] = hydro
+    table['Solar %'] = solar
+    table['Wind %'] = wind
+    table['Coal %'] = coal
+    table['Gas %'] = gas
+    table['Nuclear %'] = nuclear
+    table['Imports %'] = imports
+    table['Other %'] = other
+    
+    del table['Energy Mix']
+    return table
+
+def run_pipeline(start, end):
+    api_call = extract_api_data(start_date= start, end_date= end)
+    json_extract = extract_json_data(dataset= api_call)
+    json_transform = transform_json(table= json_extract)
+    json_transform_energy = transform_json_energy(table= json_transform)
+    
+    return json_transform_energy
+
+if __name__ == '__main__':
+    from time import time
+    run_start = time()
+    dataset = run_pipeline(start = dt.date(2022, 1, 1), end = dt.date(2022, 4, 1))
+    print(f'Run time: {round(time() - run_start, 1)}s')
